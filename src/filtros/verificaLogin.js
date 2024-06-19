64e949495777496dfd506c562f8f04c4bc15a399
majuss8 <mjuliamary0@gmail.com>
2024-06-19 20:01:03 -0300

    autorizacao

--- src/filtros/verificaLogin.js
+++ src/filtros/verificaLogin.js
@@ -5,33 +5,26 @@ const senhaHash = require('../senhaHash');
 const verificaLogin = async (req, res, next) => {
     const { authorization } = req.headers;
 
-    if (!authorization) {
+    if (!authorization || !authorization.startsWith('Bearer ')) {
         return res.status(401).json('Não autorizado');
     }
 
     try {
         const token = authorization.replace('Bearer ', '').trim();
-
-        const { id } = jwt.verify(token, senhaHash);
-
-        // const query = 'select * from usuarios where id = $1';
-        // const { rows, rowCount } = await conexao.query(query, [id]);
+        const { id } = jwt.verify(token, senhaHash); // Verifica o token usando o segredo 'senhaDaApiMarketPlace'
 
         const usuarioEncontrado = await knex('usuarios').where({ id }).first();
 
         if (!usuarioEncontrado) {
-            return res.status(404).json('Usuario não encontrado');
+            return res.status(404).json('Usuário não encontrado');
         }
 
-        const { senha, ...usuario } = usuarioEncontrado;
-
-        req.usuario = usuario;
-
+        req.usuario = usuarioEncontrado; // Define req.usuario com os dados do usuário autenticado
         next();
     } catch (error) {
-        console.log(error);
-        return res.status(400).json(error.message);
+        console.error(error);
+        return res.status(401).json('Token inválido');
     }
 }
 
-module.exports = verificaLogin
+module.exports = verificaLogin;
